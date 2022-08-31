ccfef732c44bb42aa166b3d7688d9df0720101bc
pthkbs123 <81084949+pthkbs123@users.noreply.github.com>
2022-08-31 09:10:08 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
 const startButton = document.querySelector('.start_button');
 const loading = document.querySelector('.loading');
+const result = document.querySelector('.result')
 
-function display_none(){
+const fieldValue = document.querySelector('#field_value');
+const fieldResult = document.querySelector('.field_result')
+
+const timeValue = document.querySelector('#time_value');
+const timeResult = document.querySelector('.time_result')
+
+function after_click(){
+    
+    
+    
     loading.style.display = 'none';
+    result.style.display = 'flex';
+    fieldResult.innerText = fieldValue.value;
+    timeResult.innerText = timeValue.value;
 }
 
 function click_start_button(){
+    if (fieldValue.value == ""){
+        alert('입력되지 않았습니다.');
+        fieldValue.focus();
+        return false;
+    }
+    else if (timeValue.value == ""){
+        alert('시간을 입력해 주세요.');
+        timeValue.focus();
+        return false;
+    }
+    else if (timeValue.value > 24){
+        alert('시간을 24이하의 값을 입력해 주세요');
+        timeValue.focus();
+        return false;
+    }
+    
     loading.style.display = 'flex';
-    setInterval(display_none, 15000);
+    result.style.display = 'none';
+    setInterval(after_click, 1500);
 }
 
 startButton.addEventListener("click", click_start_button)
