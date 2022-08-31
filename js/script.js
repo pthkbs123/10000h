1811b0ca3db318086bca21f55cb917ad2931f80e
pthkbs123 <81084949+pthkbs123@users.noreply.github.com>
2022-08-31 14:18:55 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const startButton = document.querySelector('.start_button');
+//const modalButton = 
+const shareButton = document.querySelector('.copy_url');
 const loading = document.querySelector('.loading');
 const result = document.querySelector('.result')
 
@@ -10,17 +12,19 @@ const timeResult = document.querySelector('.time_result')
 
 function after_click(){
     
-    
-    
     loading.style.display = 'none';
     result.style.display = 'flex';
+    
     fieldResult.innerText = fieldValue.value;
-    timeResult.innerText = timeValue.value;
+    
+    const dayValue = parseInt (10000 / timeValue.value);
+    timeResult.innerText = dayValue;
 }
 
 function click_start_button(){
     if (fieldValue.value == ""){
-        alert('입력되지 않았습니다.');
+        
+        alert('분야를 입력해 주세요.');
         fieldValue.focus();
         return false;
     }
@@ -40,5 +44,18 @@ function click_start_button(){
     setInterval(after_click, 1500);
 }
 
-startButton.addEventListener("click", click_start_button)
+function copy_url(){
+    let url = window.location.href;
+    let tmp = document.createElement('input');
+    
+    document.body.appendChild(tmp);
+    tmp.value = url;
+    tmp.select();
+    document.execCommand("copy");
+    document.body.removeChild(tmp);
+    
+    alert('url이 복사되었습니다');
+}
 
+startButton.addEventListener("click", click_start_button);
+shareButton.addEventListener("click", copy_url);
